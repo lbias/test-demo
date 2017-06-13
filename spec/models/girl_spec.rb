84e7d3edc0d2b6cee49972671b8ac64264291588
lbias <306508354@qq.com>
2017-06-13 16:43:54 +0800

    case3 single and beautiful for suit_my_taste?

--- spec/models/girl_spec.rb
+++ spec/models/girl_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe Girl, type: :model do
     girl = Girl.new(params)
     expect(girl.suit_my_taste?).to eq(true)
   end
+
+  # 测试用例3
+  # 创建了一个 既单身又漂亮的 女孩, 来验证 suit_my_taste? 方法
+  it "suit my taste? {single but not beautiful}" do
+    params = { single: true, beautiful: true }
+    girl = Girl.new(params)
+    expect(girl.suit_my_taste?).to eq(true)
+  end  
 end
