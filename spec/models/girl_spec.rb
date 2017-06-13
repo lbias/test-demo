2915b721ac341361652283bbe2e92f8948e2a84a
lbias <306508354@qq.com>
2017-06-13 16:51:41 +0800

    use before to refactor girl_spec

--- spec/models/girl_spec.rb
+++ spec/models/girl_spec.rb
@@ -1,27 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Girl, type: :model do
-  # 测试用例1
+
+  before(:all) do
+    params_1 = { single: true, beautiful: false }
+    params_2 = { single: true, beautiful: true }
+    @girl_1 = Girl.new(params_1)
+    @girl_2 = Girl.new(params_2)
+  end
+
+  # 测试用例1-1
   # 创建一个 单身 不漂亮的 女孩, 来验证 have_chance? 方法
   it "have chance? {single but not beautiful}" do
-    params = { single: true, beautiful: false }
-    girl = Girl.new(params)
-    expect(girl.have_chance?).to eq(true)
+    expect(@girl_1.have_chance?).to eq(true)
   end
 
-  # 测试用例2
+  # 测试用例1-2
   # 创建一个 单身 不漂亮的 女孩, 来验证 suit_my_taste? 方法
   it "suit my taste? {single but not beautiful}" do
-    params = { single: true, beautiful: false }
-    girl = Girl.new(params)
-    expect(girl.suit_my_taste?).to eq(true)
+    expect(@girl_1.suit_my_taste?).to eq(false)
   end
 
-  # 测试用例3
+  # 测试用例2-1
+  # 创建了一个 单身 但不漂亮的 女孩, 来验证 have_chance? 方法
+  it "have chance? {single but not beautiful}" do
+    expect(@girl_2.have_chance?).to eq(true)
+  end
+
+  # 测试用例2-2
   # 创建了一个 既单身又漂亮的 女孩, 来验证 suit_my_taste? 方法
   it "suit my taste? {single but not beautiful}" do
-    params = { single: true, beautiful: true }
-    girl = Girl.new(params)
-    expect(girl.suit_my_taste?).to eq(true)
+    expect(@girl_2.suit_my_taste?).to eq(true)
   end  
 end
