17842f6df1fd23330678cef71ca7f7a927bba20d
lbias <306508354@qq.com>
2017-06-13 16:38:30 +0800

    case1, single but not beautiful

--- spec/models/girl_spec.rb
+++ spec/models/girl_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Girl, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # 测试用例1
+  # 创建一个 单身 不漂亮的 女孩, 来验证 have_chance? 方法
+  it "have chance? {single but not beautiful}" do
+    params = { single: true, beautiful: false }
+    girl = Girl.new(params)
+    expect(girl.have_chance?).to eq(true)
+  end
 end
